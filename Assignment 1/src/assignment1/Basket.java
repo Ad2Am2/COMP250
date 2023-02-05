fa136051c72c3175b7e7554ce0b36f0293a2bfc0
Ad2Am2 <92311639+Ad2Am2@users.noreply.github.com>
2023-02-05 15:54:40 -0500

    Add Basket class

---
+++ Assignment 1/src/assignment1/Basket.java
@@ -0,0 +1,83 @@
+package assignment1;
+
+public class Basket {
+
+    private Reservation[] reservations;
+
+    public Basket() {
+        this.reservations = new Reservation[0];
+    }
+
+    public Reservation[] getProducts() {
+        Reservation[] output = new Reservation[reservations.length];
+
+        for (int i = 0; i < reservations.length; i++) {
+            output[i] = reservations[i];
+        }
+
+        return output;
+    }
+
+    public int add(Reservation reservation) {
+
+        Reservation[] newReservations = new Reservation[reservations.length + 1];
+
+        for (int i = 0; i < reservations.length; i++) {
+            newReservations[i] = reservations[i];
+        }
+
+        newReservations[reservations.length] = reservation;
+
+        this.reservations = newReservations;
+
+        return newReservations.length;
+    }
+
+
+    public boolean remove(Reservation reservation) {
+
+        int index = -1;
+
+        for (int i = 0; i < reservations.length; i++) {
+            if (reservation.equals(reservations[i])) {
+                index = i;
+            }
+        }
+
+        if (index == -1) return false;
+
+        Reservation[] newReservations = new Reservation[reservations.length - 1];
+
+        for (int i = 0; i < reservations.length; i++) {
+            if (i < index) {
+                newReservations[i] = reservations[i];
+            } else if (i >= index) {
+                newReservations[i] = reservations[i+1];
+            }
+
+        }
+
+        this.reservations = newReservations;
+
+        return true;
+    }
+
+    public void clear() {
+        reservations = new Reservation[0];
+    }
+
+    public int getNumOfReservations() {
+        return reservations.length;
+    }
+
+    public int getTotalCost() {
+        int cost = 0;
+
+        for (int i = 0; i < reservations.length; i++) {
+            cost += reservations[i].getCost();
+        }
+
+        return cost;
+    }
+
+}
