24c9ff615cfba31222a9292c6ac451ad0445f917
Ad2Am2 <92311639+Ad2Am2@users.noreply.github.com>
2023-02-04 20:26:55 -0500

    Add BnBReservation class

---
+++ Assignment 1/src/assignment1/BnBReservation.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class BnBReservation extends HotelReservation {
+
+    public BnBReservation(String name, Hotel hotel, String roomType, int numNights) {
+        super(name, hotel, roomType, numNights);
+    }
+
+    public int getCost() {
+        return super.getCost()+10;
+    }
+
+}
